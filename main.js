94e9422c2dd6f4948376a2ccf71750bb5818a2f2
mpoapostolis <mpoapostolis@gmail.com>
2017-07-12 00:51:43 +0300

    electron nsis install

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 const http = require('http');
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+const { app, BrowserWindow } = require('electron');
 
 const server = http.createServer((req, res) => {
   if (req.method == 'POST') {
-    var body = '';
+    let body = '';
     req.on('data', data => {
-      body += data;
-      console.log(body);
+      console.log(JSON.parse(data));
+      fs.writeFile(path.join(os.tmpdir(), 'data.pdf'), data, err => console.log(err));
+      exec(`gs.exe ${path.join(os.tmpdir(), 'data.pdf')}`)
     });
   }
   res.end();
 });
 
 server.listen(8000);
+
+let mainWindow;
+const createWindow = () => {
+  mainWindow = new BrowserWindow({ resizable: false, frame: false, width: 10, height: 10, x: -10, y: -250 });
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
